27eb3a2a1002630681b7a816a01f5bd59c41448b
stharding <stharding@gmail.com>
2013-09-15 10:02:34 -0600

    updated install script

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+cd ~/
+
 if [[ ! -d "~/.oh-my-zsh" ]]; then
   https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
 
+if [[ ! -d "~/.vim" ]]; then
+  curl http://cs.unm.edu/~stharding/.vim.tgz -o .vim.tgz
+  tar zxf .vim.tgz
+  rm .vim.tgz
+fi
+
 git clone git@github.com:stharding/dotfiles.git ~/.dotfiles
 
-cd ~/
-for i in `\ls -a -1 ~/.dotfiles/`; do
+for i in `\ls -ad1 ~/.dotfiles/\.* | grep -v -e ".git"`; do
   mv $i $i.old 2>/dev/null
   ln -s .dotfiles/$i . 2>/dev/null
 done
