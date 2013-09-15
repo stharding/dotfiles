0dee552e179674225e09291cc29b7d8fed13f3dd
stharding <stharding@gmail.com>
2013-09-15 11:53:13 -0600

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-DOTDIR="~/.dotfiles/"
+cd $(dirname "$0")
+DOTDIR="$(pwd)/"
 
 if [[ ! -z $1 ]]; then
   DOTDIR="$1"
@@ -8,17 +9,19 @@ fi
 
 cd ~/
 
-if [[ ! -d "~/.oh-my-zsh" ]]; then
+if [[ ! -d ~/.oh-my-zsh ]]; then
   git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
 
-if [[ ! -d "~/.vim" ]]; then
+if [[ ! -d ~/.vim ]]; then
   curl http://cs.unm.edu/~stharding/.vim.tgz -o .vim.tgz
   tar zxf .vim.tgz
   rm .vim.tgz
 fi
 
 for i in `\ls -ad1 $DOTDIR\.* | grep -v -e ".git"`; do
-  mv $i $i.old 2>/dev/null
-  ln -s .dotfiles/$i . 2>/dev/null
+#for i in `\ls -ad1 $DOTDIR\.*`; do
+  j=$(basename $i)
+  mv $j $j.old 2>/dev/null
+  ln -s .dotfiles/$j . 2>/dev/null
 done
